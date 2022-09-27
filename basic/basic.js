5f916b732abedfe6458be0f188999afbd188fe33
Jimmy CHAVES <jimmybakerchaves@gmail.com>
2022-09-27 14:22:13 +0200

    basic done

--- basic/basic.js
+++ basic/basic.js
@@ -9,25 +9,32 @@ function print(){
     let job = document.getElementById("job").value;
 
     let classElement = "";
+    let classElement2 = "";
     let count = myName.trim().length;
     console.log(myName.trim().length);
 
-    if ( count <= 5 ){
-            classElement = "red" }
-    else{
-            classElement = "green"
+        if (job == "IT"){
+                classElement2 = "bg-yellow"
+        }
+        else{
+                classElement2 = "bg-other"
         };
 
-  /*   if (job == "IT"){
-            classElement = "bg-yellow"
-    }
-    else{
-                classElement = "bg-other"
-    }; */
+        if ( count <= 5 ){
+                classElement = "red"
+                }
+        else{
+                classElement = "green"
+                };
+
+    
 
 document.getElementById("result").innerHTML = (`Name: ${myName} <br> Surname: ${surname} <br>Age:${age} <br>Job:${job}`);
 
+document.getElementById("main").setAttribute("class", classElement2);
 document.getElementById("result").setAttribute("class", classElement);
+
+
 }
 
 let result = document.getElementById("submit");
